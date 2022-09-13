0d801e59ac7a87791f3cecfb9e25d0d89cd9efb2
adaedozie <111164110+adaedozie@users.noreply.github.com>
2022-09-13 03:33:47 +0100

    print combination 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,12 +15,14 @@ for (b = a + 1; b <= 99; b++)
 {
 putchar((a / 10) + '0');
 putchar((a % 10) + '0');
+
 putchar(' ');
+
 putchar((b / 10) + '0');
 putchar((b % 10) + '0');
 
 if (a == 98 && b == 99)
-continue;
+break;
 
 putchar(',');
 putchar(' ');
