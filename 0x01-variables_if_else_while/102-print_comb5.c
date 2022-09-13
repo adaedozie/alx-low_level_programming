a91479c12dffd624d9e00d026a0e91ab701d5291
adaedozie <111164110+adaedozie@users.noreply.github.com>
2022-09-13 03:26:29 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,25 @@
 
 int main(void)
 {
-  int a, b;
-  for (a = 0; b <= 98; a++)
-    {
-      for (b = a + 1; b <= 99; b++)
-	{
-	  putchar((a / 10) + '0');
-	  putchar((a % 10) + '0');
-	  putchar(' ');
-	  putchar((b / 10) + '0');
-	  putchar((b % 10) + '0');
+int a, b;
+for (a = 0; b <= 98; a++)
+{
+for (b = a + 1; b <= 99; b++)
+{
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
+putchar(' ');
+putchar((b / 10) + '0');
+putchar((b % 10) + '0');
 
-	  if (a == 98 && b == 99)
-	    continue;
+if (a == 98 && b == 99)
+continue;
 
-	  putchar(',');
-	    putchar(' ');
+putchar(',');
+putchar(' ');
 
-	    }
-    }
-  putchar('\n');
-  return(0);
+}
+}
+putchar('\n');
+return(0);
+}
