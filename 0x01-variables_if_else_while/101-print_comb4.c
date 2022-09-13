67712383790d655666ce18d134bec396fcd4b39f
adaedozie <111164110+adaedozie@users.noreply.github.com>
2022-09-13 02:59:53 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,14 @@
 
 int main(void)
 {
-int a, b, c
+int a, b, c;
 for (a = 0; a < 8; a++)
 {
+for (b = a +1; b < 9; b++)
+{
 for (c = b + 1; c < 10; c++)
-putchar((a % 10) +'0');
+{
+putchar((a % 10) + '0');
 putchar((b % 10) + '0');
 putchar((c % 10) + '0');
 
@@ -21,6 +24,8 @@ continue;
 
 putchar(',');
 putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
