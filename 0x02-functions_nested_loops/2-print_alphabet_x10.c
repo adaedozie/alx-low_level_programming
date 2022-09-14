0973646c1c3b6ac7aa45be00caa8b8c5c51302fb
adaedozie <111164110+adaedozie@users.noreply.github.com>
2022-09-15 00:55:24 +0100

    print alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,8 @@ void print_alphabet_x10(void)
 	char letter;
 	int count = 0;
 
-	while (count++ <=9)
-		for (letter = 'a'; letter <='z'; letter++)
+	while (count++ <= 9)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-	_putchar('\n')
+	_putchar('\n');
 }
-
