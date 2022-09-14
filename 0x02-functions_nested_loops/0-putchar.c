eb1664c2aede099999449239d383ae9c11777ded
adaedozie <111164110+adaedozie@users.noreply.github.com>
2022-09-14 23:33:53 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /***
  *main - program that prints _putchar, followed by a new line
  *
@@ -16,6 +16,7 @@ int main(void)
   _putchar('a');
   _putchar('r');
   _putchar('\n');
+  
   return (0);
 
 }
