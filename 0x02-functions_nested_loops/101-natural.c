7a5097581dc8ad20d20c9600385c6130f8b1d95e
adaedozie <111164110+adaedozie@users.noreply.github.com>
2022-09-15 05:15:35 +0100

    natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	int n, sum = o;
+	int n, sum = 0;
 
 	for (n = 0; n < 1024; n++)
 	{
 		if ((n % 3) == 0 || (n % 5) == 0)
-			sum += i;
+			sum += n;
 	}
 	printf("%d\n", sum);
 	return (0);
