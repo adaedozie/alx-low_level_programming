f2d90d396f0891437699b389a475980e6a35b2b9
adaedozie <111164110+adaedozie@users.noreply.github.com>
2022-09-15 04:02:03 +0100

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,10 +7,12 @@
 void times_table(void)
 {
 	int a, b, c
-		for (a = 0; a <=9; a++)
-			_putchar('0);
+		for (a = 0; a <= 9; a++)
 	{
-		for (b = 1; b <=9; b++)
+			_putchar('0');
+	}
+	{
+		for (b = 1; b <= 9; b++)
 			_putchar(',');
 			_putchar(' ');
 
