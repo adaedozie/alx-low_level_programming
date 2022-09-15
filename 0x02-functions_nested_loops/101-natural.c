17ddc8c8203abe456fceb9eb9dac28e72e508f0b
adaedozie <edoziechibogwu@gmail.com>
2022-09-14 21:11:18 -0700

    times table

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * main - list numbers below 10that are multiles of 3 0r 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n, sum = o;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+			sum +=i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
