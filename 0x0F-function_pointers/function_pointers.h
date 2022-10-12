43aab54a7b1c02fef34a962d587350ed08bfba32
adaedozie <111164110+adaedozie@users.noreply.github.com>
2022-10-12 19:48:51 +0100

    Header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/*
- * Desc: Header file containing prototypes for all functions
- *
-/
-
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
